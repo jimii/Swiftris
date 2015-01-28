ce685c02d1f1d46df9513f72afcedbc39ec5291b
jimi <lukasz.golebiowski@icloud.com>
2015-01-28 19:45:08 +0100

    Block Party

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,10 +8,27 @@
 
 import SpriteKit
 
+let  TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
 
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
+    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        
+        if lastTick == nil {
+            return
+        }
+        
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate()
+            tick?()
+        }
+        
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -27,5 +44,13 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+    }
+    
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
